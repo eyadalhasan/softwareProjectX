63e04b0e485f5e844b3da3daa272c2c6ecc59199
eyadalhasan <eyad.haker@gmail.com>
2022-12-07 20:13:36 +0200

    eee

--- src/main/java/clinicSys/Doctor.java
+++ src/main/java/clinicSys/Doctor.java
@@ -14,5 +14,12 @@ public class Doctor extends Users {
 	public void setAvailability(boolean isAvailable) {
 		this.isAvailable = isAvailable;
 	}
+	@Override
+    public boolean equals(Object o) {
+        if  (!(o instanceof Doctor))
+            return false;
+        Doctor other = (Doctor) o;
+        return (this.userName.equals(other.userName) && this.password.equals(other.password) && this.role.equals(other.role));
+    }
 
 }
